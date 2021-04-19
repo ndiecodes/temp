ef42f288e0b6f4bade4495fda6910163312b970a
Solomon Eseme <kaperskyguru@gmail.com>
2021-04-19 17:29:14 +0100

    Added Courses, Single Course and edits

--- components/Header.vue
+++ components/Header.vue
@@ -2,28 +2,7 @@
   <header class="bg__primary header">
     <div class="container pt-2">
       <Nav />
-
-      <div class="row banner">
-        <div class="col-md-6 pt-5 mt-5 pb-4">
-          <h1>Become a Certified Crypto Trader</h1>
-
-          <p class="mt-4">
-            Everything you need to know to earn passive income from
-            cryptocurrency
-          </p>
-          <a href="#" class="btn btn-light btn__round rounded-pill px-4 mt-4"
-            >Browse Courses</a
-          >
-        </div>
-
-        <div class="col-md-6">
-          <img
-            src="/img/undraw_revenue_3osh 1.png"
-            class="site_img img-fluid"
-            alt=""
-          />
-        </div>
-      </div>
+      <Banner />
     </div>
   </header>
 </template>
